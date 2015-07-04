d98514335a05107505c23d5b6f094924e41d1ca5
unknown <c13@verna38.parinati.local>
2015-07-04 10:06:59 +0530

    bearer-auth

---
+++ api/models/User.js
@@ -0,0 +1,37 @@
+var User = {
+  // Enforce model schema in the case of schemaless databases
+  attributes: {
+   username  : { type: 'string', unique: true },
+   email     : { type: 'string',  unique: true },
+   passports : { collection: 'Passport', via: 'user' }
+   }
+
+  /*tableName: 'users',
+  connection:'TestDB',
+  autoCreatedAt:false,
+  autoUpdatedAt:false,
+  schema: true,
+
+  attributes: {
+    loginid:{
+      type:'string',
+      required:true
+    },
+    password:{
+      type:'string',
+      required:true
+    },
+    passports : {
+      collection: 'Passport',
+      via: 'user'
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  }*/
+};
+
+module.exports = User;
+
